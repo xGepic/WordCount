89cceb94b7f13845eb96212abd166bde2ab41888
AlexanderPloechl <if19b086@technikum-wien.at>
2022-11-07 11:03:28 +0100

    prototype for sorting a dictionary

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ if (len(sys.argv) != 3):
 path = sys.argv[1]
 fileExtension = sys.argv[2]
 
+testdictionary = {'jack': 4098, 'guido': 4127, 'irv': 49927}
+for words in sorted(testdictionary, key=testdictionary.get, reverse=True):
+    print(words, testdictionary[words])
 
 endTimeInMS = time.time()*1000
 print('{:3.5f}ms'.format(endTimeInMS-startTimeInMS))
