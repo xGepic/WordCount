9322ce32dd4e836a463e42d9a10f16d077e9889a
Steve <simanek.s@gmx.at>
2022-11-07 11:02:28 +0100

    removed trash

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import time
 import sys
 
-start = time.time()
+startTimeInMS = time.time()*1000
 
 # Check for Number of Arguments
 if (len(sys.argv) != 3):
@@ -12,12 +12,5 @@ path = sys.argv[1]
 fileExtension = sys.argv[2]
 
 
-numberOfWords = 0.0
-with open(r'testFolder/book.txt', 'r') as file:
-    data = file.read()
-    lines = data.split()
-    numberOfWords += len(lines)
-
-print(int(numberOfWords))
-end = time.time()
-print('{:5.3f}s'.format(end-start))
+endTimeInMS = time.time()*1000
+print('{:3.5f}ms'.format(endTimeInMS-startTimeInMS))
