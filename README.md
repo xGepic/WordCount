25053f5aa1e9e15ce955cb1c5aebe3c7b74bd192
Steve <simanek.s@gmx.at>
2022-11-08 15:17:33 +0100

    reading multiple files from dir kinda works

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # WordCount
 
 By Stefan Simanek & Alexander Plöchl
+
+In the project directory, you can run:
+
+### `python main.py [path] [fileExtension]`
+
+Example: "python main.py testFolder/ .txt"
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import pandas as pd
+from pathlib import Path
 import time
 import sys
+import numpy as np
+import re
 
-startTimeInMS = time.time()*1000
+start = time.time()
 
 # Check for Number of Arguments
 if (len(sys.argv) != 3):
@@ -11,9 +15,13 @@ if (len(sys.argv) != 3):
 path = sys.argv[1]
 fileExtension = sys.argv[2]
 
-testdictionary = {'jack': 4098, 'guido': 4127, 'irv': 49927}
-for words in sorted(testdictionary, key=testdictionary.get, reverse=True):
-    print(words, testdictionary[words])
+# Read all files in directory tree filtered by extension
+for p in Path(path).glob('**/*'+fileExtension):
+    lowerWords = [re.sub(r"[-\[\]()\"#$%/@;:<>{}'*`+=~|.!?,\n]", ' ', _)
+                  for _ in [x.lower() for x in (p.read_text().split())]]
+    lowerWords = [s.strip() for s in lowerWords]
+    values = pd.value_counts(np.array(lowerWords))
+    print(values)
 
-endTimeInMS = time.time()*1000
-print('{:3.5f}ms'.format(endTimeInMS-startTimeInMS))
+end = time.time()
+print(f'Time: {((end-start)*1000):3.5f}ms')
