8949973dd948af1ac68e4675a3ac71f52eeb5c48
AlexanderPloechl <if19b086@technikum-wien.at>
2022-11-14 11:10:20 +0100

    output jzt in extra file

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
+import re
 import pandas as pd
 from pathlib import Path
 import time
 import sys
 import numpy as np
-import re
 
 start = time.time()
 
@@ -16,12 +16,22 @@ path = sys.argv[1]
 fileExtension = sys.argv[2]
 
 # Read all files in directory tree filtered by extension
+# for p in Path(path).glob('**/*'+fileExtension):
+#     lowerWords = [re.sub(r"[-\[\]()\"#$%/@;:<>{}'*`+=~|.!?,\n]", ' ', _)
+#                   for _ in [x.lower() for x in (p.read_text().split())]]
+#     lowerWords = [s.strip() for s in lowerWords]
+#     values = pd.value_counts(np.array(lowerWords))
+#     print(values)
 for p in Path(path).glob('**/*'+fileExtension):
-    lowerWords = [re.sub(r"[-\[\]()\"#$%/@;:<>{}'*`+=~|.!?,\n]", ' ', _)
-                  for _ in [x.lower() for x in (p.read_text().split())]]
-    lowerWords = [s.strip() for s in lowerWords]
+    words = re.sub(r"[-\[\]()\"#$%/@;:<>{}'*`+=~|.!?,\n]",
+                   ' ', p.read_text()).split()
+    lowerWords = [word.lower() for word in words]
     values = pd.value_counts(np.array(lowerWords))
-    print(values)
+    # print(values)
+
+f = open("outputs/"+sys.argv[1]+".txt", "w")
+f.write(values.to_string())
+f.close()
 
 end = time.time()
 print(f'Time: {((end-start)*1000):3.5f}ms')
