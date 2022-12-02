6eeb3564828f55dca10b4d89bef476c8100010cb
Steve <simanek.s@gmx.at>
2022-12-02 12:47:49 +0100

    We did it

--- main.py
+++ main.py
@@ -1,37 +1,19 @@
-import re
-import pandas as pd
-from pathlib import Path
-import time
 import sys
+import time
+import pandas as pd
 import numpy as np
+import re
+from pathlib import Path
 
 
-def ReadArguments():
-    # Check for Number of Arguments
-    if (len(sys.argv) != 3):
-        sys.exit("Invalid Number of Arguments")
-    # Parse Arguments
-    arguments = dict(path=sys.argv[1], fileExtension=sys.argv[2])
-    return arguments
+path = sys.argv[1]
+fileExtension = sys.argv[2]
+words = []
 
 
-def ReadFiles(_arguments):
-    # Read all files in directory tree filtered by extension
-    for p in Path(_arguments["path"]).glob('**/*'+_arguments["fileExtension"]):
-        words = re.sub(r"[-\[\]()\"#$%/@;:<>{}'*`+=~|.!?,\n]",
-                       ' ', p.read_text()).split()
-        lowerWords = [word.lower() for word in words]
-        lowerWords = [s.strip() for s in lowerWords]
-        values = pd.value_counts(np.array(lowerWords))
-    parameters = dict(arguments=_arguments, values=values)
-    return parameters
-
-
-def WriteToOutputFile(_parameters):
-    # Write to File
-    f = open("outputs/"+_parameters["arguments"]["path"]+".txt", "w")
-    f.write(_parameters["values"].to_string())
-    f.close()
+def GetArguments():
+    if(len(sys.argv) != 3):
+        sys.exit("Invalid Number of arguments!")
 
 
 def PrintTime():
@@ -39,32 +21,20 @@ def PrintTime():
 
 
 start = time.time()
-WriteToOutputFile(ReadFiles(ReadArguments()))
+GetArguments()
+
+for p in Path(path).rglob('**/*'+fileExtension):
+    words.append(p.read_text().split())
+    flat_list = list(np.concatenate(words).flat)
+    new_list = [re.split(r"[^A-Za-z0-9]+", x) for x in flat_list]
+    data = list(np.concatenate(new_list).flat)
+    data = [x for x in data if len(x) >= 1]
+
+values = pd.value_counts(np.array(data))
+
+f = open("outputs/"+sys.argv[1]+".txt", "w")
+f.write(values.to_string())
+f.close()
+
 end = time.time()
 PrintTime()
-
-
-#start = time.time()
-# # Check for Number of Arguments
-# if (len(sys.argv) != 3):
-#     sys.exit("Invalid Number of Arguments")
-
-# # Parse Arguments
-# path = sys.argv[1]
-# fileExtension = sys.argv[2]
-
-# # Read all files in directory tree filtered by extension
-# for p in Path(path).glob('**/*'+fileExtension):
-#     words = re.sub(r"[-\[\]()\"#$%/@;:<>{}'*`+=~|.!?,\n]",
-#                    ' ', p.read_text()).split()
-#     lowerWords = [word.lower() for word in words]
-#     lowerWords = [s.strip() for s in lowerWords]
-#     values = pd.value_counts(np.array(lowerWords))
-
-# # Write to File
-# f = open("outputs/"+sys.argv[1]+".txt", "w")
-# f.write(values.to_string())
-# f.close()
-
-# end = time.time()
-# print(f'Time: {((end-start)*1000):3.5f}ms')
--- testFolder/test2.md
+++ testFolder/test2.md
@@ -1,4 +1,4 @@
-asdfasdf
+asdfasdf--asdf
 asdf
-asdfasdf
+Asdfasdf
 asdf
