9af90ba8ac4e9520146b45726473d3013a6caba2
AlexanderPloechl <if19b086@technikum-wien.at>
2022-12-03 17:30:52 +0100

    update

--- run.bat
+++ run.bat
@@ -4,18 +4,26 @@
 :: Shows the message on a clean line disabling the display prompt. Usually, this line goes at the beginning of the file.
 @ECHO OFF
 
-:: Prints the text after the space on the screen.
-@ECHO ===================================
-@ECHO Running: main.py testFolder .txt
-@ECHO ===================================
+:: Creates variables
 @set path=testFolder
 @set ext=.txt
+
+:: Prints the text after the space on the screen.
+@ECHO ===================================
+@ECHO Running: main.py %path% %ext%
+@ECHO ===================================
+
+:: Runs the program with arguments 'path' and 'ext'
 "%~dp0main.py" path ext
+
 @ECHO ===================================
 @ECHO Printing results: testFolder.txt
 @ECHO ===================================
+
+::Prints results to console
 @type "%~dp0outputs\testFolder.txt"
-:: newline
+
+:: Prints Newline
 @ECHO.
 :: Tells the console window to stay open after running the command. If you do not use this option, the window will close automatically as soon as the script finishes executing.
-@PAUSE 
+@PAUSE
