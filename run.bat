c52a3d1573135d6e8703702ea0593b3e9be09a5f
AlexanderPloechl <if19b086@technikum-wien.at>
2022-12-03 16:40:00 +0100

    added bat

---
+++ run.bat
@@ -0,0 +1,21 @@
+:: Prints a custom name in the title bar of the console window.
+@TITLE WordCount
+
+:: Shows the message on a clean line disabling the display prompt. Usually, this line goes at the beginning of the file.
+@ECHO OFF
+
+:: Prints the text after the space on the screen.
+@ECHO ===================================
+@ECHO Running: main.py testFolder .txt
+@ECHO ===================================
+@set path=testFolder
+@set ext=.txt
+"C:\Users\alexc\AppData\Local\Programs\Python\Python38\python.exe" "%~dp0main.py" path ext
+@ECHO ===================================
+@ECHO Printing results: testFolder.txt
+@ECHO ===================================
+@type "%~dp0outputs\testFolder.txt"
+:: newline
+@ECHO.
+:: Tells the console window to stay open after running the command. If you do not use this option, the window will close automatically as soon as the script finishes executing.
+@PAUSE 
